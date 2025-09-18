67905f8f9c910420274a3258f407d26817baafa8
Keitiely Silva <keitielysilvaviana@gmail.com>
2025-09-18 15:11:00 -0300

    Primeiro commit

---
+++ src/main/java/com/mycompany/projetoaviao/ProjetoAviao.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.projetoaviao;
+
+/**
+ *
+ * @author keity
+ */
+public class ProjetoAviao {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
